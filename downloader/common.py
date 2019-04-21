1c1632b00ec46fdefa2178a894d31a5109369393
Micah Lee <micah@micahflee.com>
2019-04-21 14:39:49 -0700

    Add some widgets

--- downloader/common.py
+++ downloader/common.py
@@ -6,12 +6,8 @@ class Common(object):
     def __init__(self):
         pass
 
-    def log(self, msg):
-        print(msg)
-
     def get_resource_path(self, filename):
         # Look for resources directory relative to python file
         prefix = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))), 'share')
         path = os.path.join(prefix, filename)
-        self.log(path)
         return path
--- downloader/main_window.py
+++ downloader/main_window.py
@@ -9,4 +9,24 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle('Downloader')
         self.setWindowIcon(QtGui.QIcon(self.common.get_resource_path('logo.png')))
 
+        label = QtWidgets.QLabel("Type a URL to download:")
+        self.lineedit = QtWidgets.QLineEdit()
+        button = QtWidgets.QPushButton("Download")
+        button.clicked.connect(self.download_clicked)
+
+        self.status_bar = QtWidgets.QStatusBar()
+        self.setStatusBar(self.status_bar)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(self.lineedit)
+        layout.addWidget(button)
+        layout.addStretch()
+
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
         self.show()
+
+    def download_clicked(self):
+        self.status_bar.showMessage("Download button clicked")
